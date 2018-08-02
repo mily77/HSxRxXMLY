6f4c7ee564e3870c0991766fb745be4bc7946201
mily77 <773258709@qq.com>
2018-08-02 16:40:04 +0800

    change headerView

---
+++ HSxRxXMLY/Classes/Main/Home/Controller/Boutique(精品)/Model/HSBoutiqueIndexModel.swift
@@ -0,0 +1,16 @@
+//
+//  HSBoutiqueIndexModel.swift
+//  HSxRxXMLY
+//
+//  Created by emily on 2018/8/2.
+//  Copyright © 2018年 emily. All rights reserved.
+//
+
+import UIKit
+
+struct HSBoutiqueIndexModel {
+    var title = ""
+    var index = 0
+    
+    
+}
--- HSxRxXMLY/Classes/Main/Home/Controller/Boutique(精品)/View/HSBoutiqueIndexHeaderView.swift
+++ HSxRxXMLY/Classes/Main/Home/Controller/Boutique(精品)/View/HSBoutiqueIndexHeaderView.swift
@@ -30,11 +30,69 @@ class HSBoutiqueIndexHeaderView: UICollectionReusableView {
     @IBOutlet weak var bottomRightView: UIView!
     
     // MARK:- 成功回调
-//    typealias AddBlock = (_ indexModel: )
+    typealias AddBlock = (_ indexModel: HSBoutiqueIndexModel)->Void
+    var didSelectItem: AddBlock? = {
+        (_) in return
+    }
+    var boutiqueIndexArr = Variable<[HSBoutiqueIndexModel]>([])
+    var modelArr: [HSBoutiqueIndexModel] = []
+    var selectedModel: HSBoutiqueIndexModel?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-    
+    static func defaultHeaderSize() ->CGSize {
+        return CGSize(width: kScreenW, height: Metric.defaultHeight)
+    }
+}
+extension HSBoutiqueIndexHeaderView {
+    private func initUI() {
+        topView.backgroundColor = kThemeWhiteColor
+        bottomLine.backgroundColor = kThemeLightGreyColor
+        collectionView.collectionViewLayout = HSBoutiqueIndexFlowLayout()
+        collectionView.register(Reusable.boutiqueIndexCell)
+    }
+    private func bindUI() {
+        boutiqueIndexArr.asObservable().subscribe(onNext: { [weak self] (boutiqueIndexArr) in
+            guard let `self` = self else { return }
+            self.modelArr = boutiqueIndexArr
+            if self.selectedModel == nil {
+                self.selectedModel = boutiqueIndexArr.first
+            }
+            self.collectionView.reloadData()
+        }).disposed(by: rx.disposeBag)
+    }
+}
+
+extension HSBoutiqueIndexHeaderView: UICollectionViewDelegate,UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.modelArr.count / 2
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeue(Reusable.boutiqueIndexCell, for: indexPath)
+        let index = self.modelArr.count / 2 * indexPath.section + indexPath.row
+        let model = self.modelArr[index]
+        if model.index == selectedModel?.index {
+            cell.bottomLine.isHidden = false
+        } else {
+            cell.bottomLine.isHidden = true
+        }
+        cell.titleLab.text = model.title
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let index = self.modelArr.count / 2 * indexPath.section + indexPath.row
+        let model = self.modelArr[index]
+        // 回调
+        didSelectItem?(model)
+        // 刷新样式
+        guard selectedModel?.index != model.index else { return }
+        selectedModel = model
+        collectionView.reloadData()
+    }
 }
